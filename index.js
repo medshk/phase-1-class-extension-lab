dcc5c9d29ba08d8ed8b6135bc44ad92a97217abf
med <mohammed.bennaceur98@gmail.com>
2023-10-09 12:51:39 +0100

    sucess

--- index.js
+++ index.js
@@ -1,1 +1,65 @@
-// Your code here
+class Polygon {
+  constructor(sides) {
+    this.sides = sides;
+  }
+
+  get countSides() {
+    return this.sides.length;
+  }
+
+  get perimeter() {
+    return this.sides.reduce((a, b) => a + b);
+  }
+}
+
+class Triangle extends Polygon {
+  constructor(sides) {
+    super(sides);
+  }
+
+  get isValid() {
+    const [a, b, c] = this.sides;
+    return a + b > c && a + c > b && b + c > a;
+  }
+}
+
+class Square extends Polygon {
+  constructor(sides) {
+    super(sides);
+  }
+
+  get isValid() {
+    return this.sides.every((side) => side === this.sides[0]);
+  }
+
+  get area() {
+    const side = this.sides[0];
+    return side * side;
+  }
+}
+
+class Triangle extends Polygon {
+  constructor(sides) {
+    super(sides);
+  }
+
+  get isValid() {
+    const [a, b, c] = this.sides;
+    return a + b > c && a + c > b && b + c > a;
+  }
+}
+
+class Square extends Polygon {
+  constructor(sides) {
+    super(sides);
+  }
+
+  get isValid() {
+    return this.sides.every((side) => side === this.sides[0]);
+  }
+
+  get area() {
+    const side = this.sides[0];
+    return side * side;
+  }
+}
